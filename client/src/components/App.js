ad93ff3368562792eed409e7499cf9b915805349
Cezary B <czarekburzykowski@gmail.com>
2018-02-01 21:30:10 +0100

    work in progress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { fetchUser } from '../actions'
 import Header from './Header'
 
 const Dashboard = () => <h2>Dashboard</h2>
@@ -7,19 +9,27 @@ const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = (props) => <h2>Landing</h2>
 
 
-const App = () => {
-  return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route path='/' component={Landing} exact={true} />
-          <Route path='/surveys' component={Dashboard} exact={true} />
-          <Route path='/surveys/new' component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path='/' component={Landing} exact={true} />
+            <Route path='/surveys' component={Dashboard} exact={true} />
+            <Route path='/surveys/new' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.loggedIn
+  }
+}
+
+export default connect(mapStateToProps, { fetchUser })(App)
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,13 @@
+import { FETCH_USER } from '../actions/types'
+
 const INITIAL_STATE = {
   loggedIn: false
 }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      return { loggedIn: true }
     default:
       return state
   }
